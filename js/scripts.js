27b3a67ab3e855cbf899811bacd3fbe4c8dcc62e
LucaDalRi <euro2xd@gmail.com>
2023-01-24 15:35:08 +0100

    Progetto base

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        done : false,
+        lista: [
+            'Fare la spesa',
+            'Portare il cane fuori',
+            'Pulire la cantina'
+        ]
+    }
+    
+  },
+  methods: {
+
+
+
+
+  }
+}).mount('#app')
